aef05b828cb871bc8e80b3c792dfa8a0f3d3e6f0
Amir <hashemi.eng1985@gmail.com>
2017-12-08 22:39:34 -0500

    Make cell for collection view and put text view and image view in it.

---
+++ Master Ielts Speaking/EachCategoryCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EachCategoryCollectionViewCell.swift
+//  Master Ielts Speaking
+//
+//  Created by seyedamirhossein hashemi on 2017-12-08.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import UIKit
+
+class EachCategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIView!
+    
+    @IBOutlet weak var backgroundViewForText: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundViewForText.layer.cornerRadius = 20
+    }
+    
+    
+    
+}
