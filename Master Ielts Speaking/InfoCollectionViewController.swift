5e82e081093602498d2cd85c67f9a62a04552bb6
new_user_name <new@email.com>
2017-12-08 22:06:55 -0500

    Add infoCollectionViewController to shoe the words of each category.

---
+++ Master Ielts Speaking/InfoCollectionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  InfoCollectionViewController.swift
+//  Master Ielts Speaking
+//
+//  Created by seyedamirhossein hashemi on 2017-12-08.
+//  Copyright © 2017 seyedamirhossein hashemi. All rights reserved.
+//
+
+import UIKit
+
+class InfoCollectionViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
