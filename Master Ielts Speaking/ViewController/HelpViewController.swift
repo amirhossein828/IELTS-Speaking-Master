43a60d4d7b0b6c46650d227d38ec84006e2f0975
Amir <hashemi.eng1985@gmail.com>
2017-12-21 11:29:36 -0500

    Add webView to HelpViewController to show the way that app works.

--- Master Ielts Speaking/ViewController/HelpViewController.swift
+++ Master Ielts Speaking/ViewController/HelpViewController.swift
@@ -7,29 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
 class HelpViewController: UIViewController {
-
+    // Outlets
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // create url
+        let url = URL(string: "https://medium.com/@hashemi.eng1985/splash-screen-with-custom-dots-framework-9ed737a17770")
+        // create request
+        let request = URLRequest(url: url!)
+        // load the page by the request
+        webView.load(request)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Back button
+    @IBAction func backButton(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
 
 }
