1299520546ef9fe414fd7e71b89200ccffd49a77
Amir <hashemi.eng1985@gmail.com>
2017-12-26 11:03:31 -0500

    Add comments.

--- Master Ielts Speaking/Model/Word.swift
+++ Master Ielts Speaking/Model/Word.swift
@@ -9,28 +9,26 @@
 import Foundation
 import RealmSwift
 
-
+/**
+ * Model class for a Word
+ *
+ * - author: Amir
+ * - version: 1
+ *
+ */
 class Word : Object {
     @objc dynamic var wordId : String = UUID().uuidString
     @objc dynamic var wordName : String = ""
     @objc dynamic var wordImage : Data? = nil
     @objc dynamic var wordImageString : String? = nil
+    // definitions of the word
     var definitions = List<String>()
+    // sentence examples of the word
     var examples = List<String>()
-    
+    // primary key 
     override class func primaryKey() -> String {
         return "wordName"
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  
 }
