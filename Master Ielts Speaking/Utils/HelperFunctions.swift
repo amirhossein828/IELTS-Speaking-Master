2f4487fd8e53431eb87a1f8ab93cdf982cb4dfe8
Amir <hashemi.eng1985@gmail.com>
2018-02-05 21:17:08 -0500

    Move two method to helpful functions.

--- Master Ielts Speaking/Utils/HelperFunctions.swift
+++ Master Ielts Speaking/Utils/HelperFunctions.swift
@@ -167,3 +167,36 @@ func ResizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
     
     return newImage!
 }
+
+func resizePhoto(image: UIImage) -> UIImage{
+    UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+    image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+    return newImage
+}
+
+func createpixelBuffer(newImage: UIImage) -> CVPixelBuffer? {
+    UIGraphicsEndImageContext()
+    
+    let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+    var pixelBuffer : CVPixelBuffer?
+    let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+    guard (status == kCVReturnSuccess) else {
+        return nil
+    }
+    
+    CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+    let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+    
+    let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+    let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+    
+    context?.translateBy(x: 0, y: newImage.size.height)
+    context?.scaleBy(x: 1.0, y: -1.0)
+    
+    UIGraphicsPushContext(context!)
+    newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+    UIGraphicsPopContext()
+    CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+    return pixelBuffer
+}
